4a8e9641e6b1832a15abd8a29781cb6d08087a7b
gassuncao-ccm <gassuncao@ccmtecnologia.com.br>
2026-02-20 20:06:53 +0000

    Just triggering the pipe, relax man

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little human!! O que sobra?? Alguém calcula?',
+        'message': 'You are doing great, little human!! O que sobra?? Alguém calcula? Nem as migalhas',
         'deployed_on': 'kubernetes'
     })
 
